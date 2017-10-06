365986aea9e9de0c87d8d65fb34c5c8b658abd75
Sourabh <Sourabh@192.168.1.3>
2017-10-06 12:44:36 +0530

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/org/testcases/TC_001.java
@@ -0,0 +1,19 @@
+package org.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	@Test
+	public void tc_001()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Hello");
+		driver.findElement(By.id("pass")).sendKeys("Hello");
+		driver.close();
+	}
+}
